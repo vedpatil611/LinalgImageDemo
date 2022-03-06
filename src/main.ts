ba2fc1a36756980858890ba38fa037d3e3a43b8d
vedpatil611 <vedpatil611@gmail.com>
2022-03-06 09:15:40 +0530

    image generation

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { decode, encode, RawImageData, BufferLike } from 'jpeg-js'
 import * as buffer from 'buffer';
 import init, {
+    IntegersMatrix,
     IntegersVector
 } from '@ml.wasm/linalg';
 
@@ -13,7 +14,8 @@ import init, {
 
 function processImage(array: Int32Array, _width: number, _height: number) : Int32Array {
     const linArr = IntegersVector.newFromTypedArray(array);
-
+    // console.log(array[0], array[1], array[2], array[3])
+    // console.log(linArr.len());
     linArr.transform(function (data: number) {
         return 256 - data;
     });
@@ -34,19 +36,34 @@ function parseImage(event: Event) {
 
     const dataUrlReader = new FileReader();
     dataUrlReader.addEventListener('load', function () {
-     (document.getElementById('inputimage') as HTMLImageElement).src = dataUrlReader.result as string;
+        (document.getElementById('inputimage') as HTMLImageElement).src = dataUrlReader.result as string;
     });
     dataUrlReader.readAsDataURL(files[0]);
 
     const arrayReader = new FileReader();
     arrayReader.addEventListener('load', function () {
-        const d = decode(arrayReader.result as ArrayBuffer);
-        const result = processImage(new Int32Array(d.data), d.width, d.height)
+        // const d = decode(arrayReader.result as ArrayBuffer);
+        // const result = processImage(new Int32Array(d.data), d.width, d.height)
+        // console.log(result.length)
+        // console.log(d.width * d.height * 4)
+
+        let buffer = IntegersMatrix.newWithZeroes(640,480 * 4);
+        for(let i = 0; i < buffer.nrows(); i++) {
+            for (let j = 0; j < buffer.ncols(); j+=4) {
+                buffer.set([i,j], 255);
+                buffer.set([i,j + 1], 0);
+                buffer.set([i,j + 2], 0);
+                buffer.set([i,j + 3], 255);
+            }
+        }
+
+        // console.log(d.width, d.height)
+
         // ENCODE TO JPEG DATA
         const resultImage: RawImageData<BufferLike> = {
-            width: d.width,
-            height: d.height,
-            data: result
+            width: buffer.nrows(),
+            height: buffer.ncols() / 4,
+            data: buffer.toVector().toTypedArray()
         }
         const encoded = encode(resultImage, 100)
 
