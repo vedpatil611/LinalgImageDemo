b860d501d1597313684813f80137f8fc9cc17225
vedpatil611 <vedpatil611@gmail.com>
2022-03-27 11:40:00 +0530

    Clean up

--- src/main.ts
+++ src/main.ts
@@ -6,64 +6,26 @@ import init, {
 
 (window as any).Buffer = buffer.Buffer;
 
-// const lightNormal = [ 0.0, 0.0, -1.0 ];
-// const ambientLight = [30, 30, 30, 255];
-
 let lightNormal: FloatsVector;
 let ambientLight: BytesVector;
+let color: BytesVector;
 
 (async () => {
     await init('node_modules/@ml.wasm/linalg/linalg_bg.wasm');
     
     lightNormal = new FloatsVector([0.0, 1.0, -1.0]);
     ambientLight = new BytesVector([50, 50, 50, 255]);
-    // document.getElementById('fileinput')!.onchange = parseImage;
-    
+    color = new BytesVector([255, 0, 0, 255]);
+
     parseImage()
 })();
 
-/* function processImage(array: Uint8Array, _width: number, _height: number) : Uint8Array {
-    // const linArr = IntegersVector.newFromTypedArray(array);
-    const linArr = BytesVector.newFromTypedArray(array);
-    // console.log(array[0], array[1], array[2], array[3])
-    // console.log(linArr.len());
-    linArr.transform(function (data: number) {
-        return 256 - data;
-    });
-
-    return linArr.toTypedArray();
-} */
-
 function parseImage() {
-    // const files = (<HTMLInputElement>event.target).files;
-
-    // if (!files || files.length < 1) {
-        // return;
-    // }
-    /* if (files[0].type != 'image/jpeg') {
-        console.log('file is not a jpeg!');
-        return;
-    } */
-
-    const dataUrlReader = new FileReader();
-    dataUrlReader.addEventListener('load', function () {
-        (document.getElementById('inputimage') as HTMLImageElement).src = dataUrlReader.result as string;
-    });
-    // dataUrlReader.readAsDataURL(files[0]);
-
-    // const arrayReader = new FileReader();
-    // arrayReader.addEventListener('load', function () {
-        // const d = decode(arrayReader.result as ArrayBuffer);
-        // const result = processImage(new Int32Array(d.data), d.width, d.height)
-        // console.log(result.length)
-        // console.log(d.width * d.height * 4)
-
     const WIDTH = 300;
     const HEIGHT = 300;
 
     let buffer = BytesMatrix.newWithZeroes(WIDTH, HEIGHT * 4);
     let zBuffer = IntegersMatrix.newWithElement(WIDTH, HEIGHT, -1000);
-    // console.log(zBuffer.get([0,0]))
 
     const cx = zBuffer.ncols() / 2;
     const cy = zBuffer.nrows() / 2;
@@ -74,16 +36,12 @@ function parseImage() {
     let int = Math.trunc;
 
     lightNormal = normalizeVec3(lightNormal);
-    console.log(lightNormal.toString());
-    // lightNormal.divConstant(lightNormal.powf(2).sum());
+    // console.log(lightNormal.toString());
 
     const rot: FloatsMatrix = xRotationMatrix(degree2rad(30)).dot(zRotationMatrix(degree2rad(30)));
     
     for(let theta = 0.0; theta < Math.PI * 2; theta += 0.01) {
         for (let phi = 0.0; phi < Math.PI * 2; phi += 0.01) {
-            // const x = R * Math.cos(theta);
-            // const y = R * Math.sin(theta);
-            
             const st = Math.sin(theta);
             const ct = Math.cos(theta);
             const sp = Math.sin(phi);
@@ -93,17 +51,7 @@ function parseImage() {
             pos.set(0, cp * (R + r * ct));
             pos.set(1, r * st);
             pos.set(2, -sp * (R + r * ct));
-
-            // const x = cp * (R + r * ct);
-            // const y = r * st;
-            // const z = - sp * (R + r * ct);
-
-            // const nx = cp * ct;
-            // const ny = st;
-            // const nz = - sp * ct;
-
-            // let nPow = nx * lightNormal.get(0) + ny * lightNormal.get(1) + nz * lightNormal.get(2);
-            
+           
             let normal = new FloatsVector([cp * ct, st, -sp * ct]);
 
             pos = mulMatVec(rot, pos);
@@ -120,72 +68,23 @@ function parseImage() {
             if (zBuffer.get([ty, tx]) < pos.get(2)) {
                 zBuffer.set([ty, tx], pos.get(2));
                 
-                let r = ambientLight.get(0) + nPow * 255;
+                let r = ambientLight.get(0) + nPow * color.get(0);
                 if (r > 255) { r = 255; }
 
-                let g = ambientLight.get(1) + nPow * 0;
+                let g = ambientLight.get(1) + nPow * color.get(1);
                 if (g > 255) { g = 255; }
  
-                let b = ambientLight.get(1) + nPow * 0;
+                let b = ambientLight.get(1) + nPow * color.get(2);
                 if (b > 255) { b = 255; }
                                
-                // buffer.set([ty, 4 * tx + 0], ambientLight[0] + nPow * 255);
                 buffer.set([ty, 4 * tx + 0], r);
                 buffer.set([ty, 4 * tx + 1], g);
                 buffer.set([ty, 4 * tx + 2], b);
                 buffer.set([ty, 4 * tx + 3], 255);
             }
-            // buffer.set([int(cy + y), 4 * int(cx + x) + 0], 255);
-            // buffer.set([int(cy + y), 4 * int(cx + x) + 1], 0);
-            // buffer.set([int(cy + y), 4 * int(cx + x) + 2], 0);
-            // buffer.set([int(cy + y), 4 * int(cx + x) + 3], 255);
        }
     }
-/*
-    const tx = cx + 0;
-    const ty = cy + 0;
-    
-    console.log(buffer.get([ty, tx]));
-    console.log(buffer.get([ty, tx + 1]));
-    console.log(buffer.get([ty, tx + 2]));
-    console.log(buffer.get([ty, tx + 3])); */
-
-    /* for (let i = 0; i < zBuffer.nrows(); i++) {
-        for (let j = 0; j < zBuffer.ncols(); j++) {
-            if ((cx - i) * (cx - i) + (cy - j) * (cy - j) <= R * R) {
-                buffer.set([i,4 * j], 255);
-                buffer.set([i,4 * j + 1], 0);
-                buffer.set([i,4 * j + 2], 0);
-                buffer.set([i,4 * j + 3], 255);
-            }
-        }
-    } */
-    
-    /* for(let i = 0; i < zBuffer.nrows(); i++) {
-        for (let j = 0; j < zBuffer.ncols(); j++) {
-            if(i > zBuffer.nrows() / 2) {
-                if(j > zBuffer.ncols() / 2) {
-                    buffer.set([i,4 * j], 255);
-                    buffer.set([i,4 * j + 1], 0);
-                    buffer.set([i,4 * j + 2], 0);
-                    buffer.set([i,4 * j + 3], 255);
-                } else {
-                    buffer.set([i,4 * j], 0);
-                    buffer.set([i,4 * j + 1], 0);
-                    buffer.set([i,4 * j + 2], 255);
-                    buffer.set([i,4 * j + 3], 255);
-                }
-            } else {
-                buffer.set([i,4 * j], 0);
-                buffer.set([i,4 * j + 1], 255);
-                buffer.set([i,4 * j + 2], 0);
-                buffer.set([i,4 * j + 3], 255);
-            }
-        }
-    } */
-
-    // console.log(buffer.toVector().toString());
-
+ 
     // ENCODE TO JPEG DATA
     const resultImage: RawImageData<BufferLike> = {
         width: buffer.nrows(),
@@ -208,8 +107,6 @@ function parseImage() {
 
     // ASSIGN DATA URL TO OUTPUT IMAGE ELEMENT
     (document.getElementById('outputimage') as HTMLImageElement).src = processed
-    // })
-    // arrayReader.readAsArrayBuffer(files[0]);
 }
 
 function degree2rad(angle: number) {
@@ -230,6 +127,16 @@ function mulMatVec(mat: FloatsMatrix, vec: FloatsVector) {
     res.set(0, mat.getR(0).mul(vec).sum());
     res.set(1, mat.getR(1).mul(vec).sum());
     res.set(2, mat.getR(2).mul(vec).sum());
+
+    return res;
+}
+
+function scalingMatrix(vec: FloatsVector) {
+    let res = FloatsMatrix.newWithZeroes(3, 3);
+
+    res.set([0, 0], vec.get(0));
+    res.set([1, 1], vec.get(1));
+    res.set([2, 2], vec.get(2));
 
     return res;
 }
